385d37b91a561b1a046d06b606bf08f09a1d7ca4
mightie <111096222+mightiemike@users.noreply.github.com>
2026-07-22 20:37:06 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,6 +2,6 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "codertjay/2026-07-metric-dev-oyakhil-main"
-REPO_NAME = "2026-07-metric-dev-oyakhil-main"
+SOURCE_REPO = "near/nearcore"
+REPO_NAME = "nearcore"
 
