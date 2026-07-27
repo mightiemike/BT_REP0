d1601596b1913b82f20c9216836f01bb3ed5a944
mightie <111096222+mightiemike@users.noreply.github.com>
2026-07-27 15:41:10 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "near/nearcore"
-REPO_NAME = "nearcore"
+MAX_REPO = 25
+SOURCE_REPO = "1inch/cross-chain-swap"
+REPO_NAME = "cross-chain-swap"
 
