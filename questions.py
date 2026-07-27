e138b3ea55a407980ed0ea947989e85aa42b862f
mightie <111096222+mightiemike@users.noreply.github.com>
2026-07-27 16:04:33 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,6 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "1inch/cross-chain-swap"
-REPO_NAME = "cross-chain-swap"
-
+SOURCE_REPO = "starkware-libs/sequencer"
+REPO_NAME = "sequencer"
+TREE = ""
+BRANCH = ""
+# Example:
+# https://github.com/pushchain/push-chain-node/tree/0648551281dada6e300f51baca0e7464cb210eef/
