fc7f12557460bfb2b37e89f448e4bd1516695e25
mightie <111096222+mightiemike@users.noreply.github.com>
2026-07-27 16:05:31 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,9 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "starkware-libs/sequencer"
-REPO_NAME = "sequencer"
-TREE = ""
+SOURCE_REPO = "pushchain/push-chain-node/tree/"
+REPO_NAME = "push-chain-node"
+TREE = "0648551281dada6e300f51baca0e7464cb210eef"
 BRANCH = ""
 # Example:
 # https://github.com/pushchain/push-chain-node/tree/0648551281dada6e300f51baca0e7464cb210eef/
