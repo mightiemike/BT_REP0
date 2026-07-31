48925d9e3135b9034ea8ed6ce6ec96fc7049f01a
mightie <111096222+mightiemike@users.noreply.github.com>
2026-07-31 19:25:18 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,9 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "pushchain/push-chain-node/tree/"
-REPO_NAME = "push-chain-node"
-TREE = "0648551281dada6e300f51baca0e7464cb210eef"
+SOURCE_REPO = "aptos-labs/aptos-core"
+REPO_NAME = "aptos-core"
+TREE = ""
 BRANCH = ""
 # Example:
 # https://github.com/pushchain/push-chain-node/tree/0648551281dada6e300f51baca0e7464cb210eef/
