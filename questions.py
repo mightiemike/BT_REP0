d8a4b4aad54deb2ff45f57d154355dbac2be8c5a
mightie <111096222+mightiemike@users.noreply.github.com>
2026-08-03 22:32:55 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "tronprotocol/java-tron"
-REPO_NAME = "java-tron"
+MAX_REPO = 25
+SOURCE_REPO = "anza-xyz/agave"
+REPO_NAME = "agave"
 TREE = ""
 BRANCH = ""
 # Example:
