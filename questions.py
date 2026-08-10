cdcf8f14ff6140fb81152abc82da80ae85cd3832
mightie <111096222+mightiemike@users.noreply.github.com>
2026-08-10 19:55:50 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "anza-xyz/agave"
-REPO_NAME = "agave"
+MAX_REPO = 25
+SOURCE_REPO = "celestiaorg/go-square"
+REPO_NAME = "go-square"
 TREE = ""
 BRANCH = ""
 # Example:
