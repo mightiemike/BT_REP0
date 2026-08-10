3d88dcce659fe3a91f13e78f48c49bdf32fdcb76
mightie <111096222+mightiemike@users.noreply.github.com>
2026-08-10 20:21:52 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "celestiaorg/go-square"
-REPO_NAME = "go-square"
+SOURCE_REPO = "smartcontractkit/chainlink"
+REPO_NAME = "chainlink"
 TREE = ""
 BRANCH = ""
 # Example:
