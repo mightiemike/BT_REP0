91fb5b289665680f59882d1e1f8675fb6dc49b36
mightie <111096222+mightiemike@users.noreply.github.com>
2026-08-25 14:10:10 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "Shopify/shopify-app-js"
-REPO_NAME = "shopify-app-js"
+SOURCE_REPO = "anza-xyz/agave"
+REPO_NAME = "agave"
 TREE = ""
 BRANCH = ""
 # Example:
