3894b3187c2ef26ca51272095ad3686229705f86
mightie <111096222+mightiemike@users.noreply.github.com>
2026-09-07 11:53:50 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "defuse-protocol/sdk-monorepo"
-REPO_NAME = "sdk-monorepo"
+SOURCE_REPO = "paradigmxyz/reth"
+REPO_NAME = "reth"
 TREE = ""
 BRANCH = ""
 # Example:
